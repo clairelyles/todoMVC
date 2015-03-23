f9a7b9df50fa8945071a75afc72d580f020ba527
clairelyles <claire.lyles@gmail.com>
2015-03-22 21:24:39 -0700

    added fixture data

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,4 +1,21 @@
 Todos.Todo = DS.Model.extend({
-  title: DS.attr('string'),
-  isCompleted: DS.attr('boolean')
+  title: DS.attr("string"),
+  isCompleted: DS.attr("boolean")
 });
+
+Todos.Todo.FIXTURES = [
+{
+  id: 1,
+  title: "Learn Ember.js this weekend",
+  isCompleted: true
+},
+{
+  id: 2,
+  title: "Pack clothes before I go to bed",
+  isCompleted: false
+},
+{
+  id: 3,
+  title: "Brush teeth before I go to bed",
+  isCompleted: false
+}]
